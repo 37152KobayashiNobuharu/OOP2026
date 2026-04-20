de58de27798abfc74ca56b0a32a69450f36f8570
伸悠 小林 <oibc37152@ojs.ac.jp>
2026-04-20 10:22:58 +0900

    繰り返し回数の表示

---
+++ Chapter00/Sample0415/Sample0415/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Sample0415
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("array[" + i + "] =");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            //int sum = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    sum += array[i];
+                for (int i = 0; i < array.Length; i++)
+                {
+                Console.Write("array[" + i + "]");
+                    astOut(array[i]);
+                }
+            Console.WriteLine("合計値:" + array.Where(n => n % 2 == 0).Sum());
+                
+            }
+            static void astOut(int num)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    Console.Write("*");
+                }
+            Console.WriteLine();
+        }
+        }
+    }
+
+
