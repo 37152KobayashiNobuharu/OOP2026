92804d2e41b0af675068abd04b80d99b21465147
伸悠 小林 <oibc37152@ojs.ac.jp>
2026-04-22 09:31:47 +0900

    コマンドライン引数の対応

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DistanceConverter
+﻿
+
+namespace DistanceConverter
 {
     internal class Program
     {
@@ -6,22 +8,38 @@
         {
             if (args.Length >= 1 && args[0] == "-tom")
             {
-                //フィートからメートルへの対応表を出力
-                for (int feet = 1; feet < 10; feet++)
-                {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine($"{feet}ft = {meter:0.000}m");
-                }
+                PrintFeetToMeterList(1,10);     //メートルへの変換
+            }
+            if (args.Length >= 1 && args[0] == "-tof")
+            {
+                PrintMeterToFeetList(1, 10);     //フィートへの変換
             }
             else
             {
-                for (int meter = 1; meter < 10; meter++)
-                {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine($"{meter}m = {feet:0.000}ft");
-                }
+                Console.WriteLine("引数エラー");
             }
         }
+
+        static void PrintFeetToMeterList(int start, int stop)
+        {
+            //フィートからメートルへの対応表を出力
+            for (int feet = start; feet <= stop; feet++)
+            {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine($"{feet}ft = {meter:0.000}m");
+            }
+        }
+        static void PrintMeterToFeetList(int start, int stop)
+        {
+            for (int meter = start; meter < stop; meter++)
+            {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine($"{meter}m = {feet:0.000}ft");
+            }
+        }
+
+
+
         //フィートからメートルを求める
         static double FeetToMeter(int feet)
         {
