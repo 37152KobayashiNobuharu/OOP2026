7d09fabc211eacd01459137612e6dc1ff0db271c
伸悠 小林 <oibc37152@ojs.ac.jp>
2026-05-11 10:26:05 +0900

    2.1.1完成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class Song {
+        public string Title { get; set; } = string.Empty;
+        public string ArtistName { get; set; } = string.Empty;
+        public int Length { get; set; }
+
+        public Song(string Title,string ArtistName,int Length) {
+            Title = this.Title;
+            ArtistName = this.ArtistName;
+            Length = this.Length;
+        }
+       
+    }
+}
