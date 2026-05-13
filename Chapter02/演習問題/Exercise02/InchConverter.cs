fe098c73e03983169ffcd08dc7949afaf63cf666
伸悠 小林 <oibc37152@ojs.ac.jp>
2026-05-13 11:31:26 +0900

    訂正

--- Chapter02/演習問題/Exercise02/InchConverter.cs
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -7,8 +7,11 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     internal class InchConverter {
         private static readonly double ratio = 0.0254;  //定数
-        public static double ToInch(double Inch) {
-            return Inch * 0.0254;
+        public static double FromMeter(double meter) {
+            return meter * ratio;
+        }
+        public static double ToMeter(double Inch) {
+            return Inch * ratio;
         }
     }
 }
--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -3,16 +3,12 @@ namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
             PrintInchToMetorList(1, 10);
-
         }
         private static void PrintInchToMetorList(int start, int end) {
             for (int Inch = start; Inch <= end; Inch++) {
-                double inch = InchConverter.ToInch(Inch);
+                double inch = InchConverter.ToMeter(Inch);
                 Console.WriteLine($"{Inch}Inch = {inch:0.000}M");
             }
-
-
-
         }
     }
 }
