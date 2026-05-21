21edb01aa335d439cc1373b06424ed1365d5d391
伸悠 小林 <oibc37152@ojs.ac.jp>
2026-05-21 09:55:04 +0900

    問題3.1.2完成

--- Chapter03/演習問題/Exercise01/Program.cs
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -28,7 +28,9 @@ namespace Exercise01 {
         }
 
         private static void Exercise2(List<int> numbers) {
-
+            foreach (var s in numbers) {
+                Console.WriteLine(s / 2.0);
+            }
         }
 
         private static void Exercise3(List<int> numbers) {
