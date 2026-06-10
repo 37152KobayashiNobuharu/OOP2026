6782652bedcb018d7802770c58ce6ff8052466d6
伸悠 小林 <oibc37152@ojs.ac.jp>
2026-06-10 10:29:52 +0900

    6.2完成

---
+++ Chapter06/演習問題/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            string input = Console.ReadLine();
+            int.TryParse(input, out int num);
+            Console.WriteLine(num.ToString("N0"));
+        }
+    }
+}
