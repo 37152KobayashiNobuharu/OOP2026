9171f4c148015c41bd1dea2a15cfd12233326a24
伸悠 小林 <oibc37152@ojs.ac.jp>
2026-06-15 10:36:09 +0900

    カンマ区切り

---
+++ Chapter06/文字列の連結を分割/Section05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+            var sb = new StringBuilder();
+            foreach (var word in GetWords()) {
+                sb.Append(word);
+            }
+            var separator = ",";
+            var result = String.Join(separator,GetWords()); 
+            Console.WriteLine(result);
+        }
+
+        private static IEnumerable<string> GetWords() {
+            return ["Orange", "Lemon", "Strawberry"];
+        }
+    }
+}
