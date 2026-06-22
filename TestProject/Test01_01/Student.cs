3f1fe011e5f689f5eb096333cac926e3f301cd23
伸悠 小林 <oibc37152@ojs.ac.jp>
2026-06-22 11:31:07 +0900

    「設問１_Studentクラス完成

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,7 +1,10 @@
 ﻿namespace Test01_01 {
     public class Student {
-        
-
-
+        //学生の名前
+        public string Name { get; init; } = string.Empty;
+        //科目名
+        public string Subject { get; init; } = string.Empty;
+        //点数
+        public int Score { get; init; }
     }
 }
