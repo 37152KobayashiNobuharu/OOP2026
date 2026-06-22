cb8f87c2976ba1f921d7272d7233f698fbc135b3
伸悠 小林 <oibc37152@ojs.ac.jp>
2026-06-22 11:54:24 +0900

    設問１_提出

--- TestProject/Test01_01/ScoreCounter.cs
+++ TestProject/Test01_01/ScoreCounter.cs
@@ -9,28 +9,33 @@
 
         //メソッドの概要：
         private static IEnumerable<Student> ReadScore(string filePath) {
-            var sales = new List<Student>();
+            var Score = new List<Student>();
             var lines = File.ReadAllLines(filePath);
-            
-
-
-
-
-
-
-
-            return sales;
+            foreach (var line in lines) {
+                var items = line.Split(',');//カンマ区切りで分割
+                var score = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                Score.Add(score);
+            }
+            return Score;
         }
+
 
         //メソッドの概要：
         public IDictionary<string, int> GetPerStudentScore() {
             var dict = new Dictionary<string, int>();
-
-
-
-
-
-
+            foreach (var Score in _score) {
+                if (dict.ContainsKey(Score.Subject)) {
+                    //登録されている場合
+                    dict[Score.Subject] += Score.Score;//売上を足しこみ
+                } else {
+                    //未登録の場合
+                    dict[Score.Subject] = Score.Score;//新規に売上を登録
+                }
+            }
             return dict;
         }
     }
