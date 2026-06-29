67e17747a12d34c292241ab562bbf03c2e87f436
伸悠 小林 <oibc37152@ojs.ac.jp>
2026-06-29 13:31:05 +0900

    上書き完成

--- Chapter08/Section01/Program.cs
+++ Chapter08/Section01/Program.cs
@@ -5,10 +5,12 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Section01 {
     internal class Program {
         static private Dictionary<string, string> prefOfficeDict = new Dictionary<string, string>();
+        static private Dictionary<string, string> flowerDict = new Dictionary<string, string>();
 
         static void Main(string[] args) {
             string? pref, prefCaptlLocation;
             var dict = new Dictionary<string, string>();
+            var flowerDict = new Dictionary<string, string>();
             Console.WriteLine("県庁所在地の登録【入力終了:Ctrl + 'Z'】");
 
 
@@ -22,10 +24,21 @@ namespace Section01 {
                 Console.Write("県庁所在地:");
                 prefCaptlLocation = Console.ReadLine();
                 if (prefCaptlLocation == null) break;
-                dict[pref] = prefCaptlLocation;
+
+                if (flowerDict.ContainsKey(pref)) {
+                    Console.WriteLine("上書きしますか？(y/n)");
+                    string? answer = Console.ReadLine();
+                    if (answer == "y") {
+                        flowerDict[pref] = prefCaptlLocation;
+                    }
+                    flowerDict[pref] = prefCaptlLocation;
+                }
+
+                flowerDict[pref] = prefCaptlLocation;
             }
             //③県庁所在地登録処理
             while (true) {
+                Console.WriteLine("メニュー");
                 Console.WriteLine("1:一覧表示");
                 Console.WriteLine("2:検索");
                 Console.WriteLine("9:終了");
@@ -33,7 +46,7 @@ namespace Section01 {
 
                 switch (input) {
                     case "1":
-                        foreach (var item in dict) {
+                        foreach (var item in flowerDict) {
                             Console.WriteLine($"{item.Key}の県庁所在地は{item.Value}です。");
                         }
 
@@ -41,11 +54,14 @@ namespace Section01 {
                     case "2":
                         Console.WriteLine("都道府県");
                         string? serch = Console.ReadLine();
-                        Console.WriteLine($"{serch}の県庁所在地は{dict[serch]}です。");
+                        if (flowerDict.ContainsKey(serch)) {
+                            Console.WriteLine($"{serch}の県庁所在地は{flowerDict[serch]}です。");
+                        }
                         break;
                     case "9":
-                        
                         return;
+                    default:
+                        break;
                 }
             }
         }
