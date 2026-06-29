4f5f48c7333e0811bacf623904910db607e152c2
伸悠 小林 <oibc37152@ojs.ac.jp>
2026-06-29 15:49:47 +0900

    第８章演習問題準備

---
+++ Chapter08/Section02/Section02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http.Headers;
+
+namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.txt");
+            var we = new WordsExtractor(lines);
+            foreach (var word in we.Extract()) {
+                Console.WriteLine(word);
+            }
+
+        }
+    }
+}
