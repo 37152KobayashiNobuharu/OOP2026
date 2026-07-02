8507c5e691ae2dfa3f3832088e95d3ff7f3b96ee
伸悠 小林 <oibc37152@ojs.ac.jp>
2026-07-02 13:20:29 +0900

    8.2.2完成

--- Chapter08/演習問題/Exercise02/Abbreviations.cs
+++ Chapter08/演習問題/Exercise02/Abbreviations.cs
@@ -36,12 +36,7 @@ public class Abbreviations {
 
 
     // 8.2.2（最終的に式形式で記述すること）
-    public bool Remove(string abb) {
-
-
-
-        return true;    //エラー回避のために記述
-    }
+    public bool Remove(string abb) => _dict.Remove(abb);
 
     // Dictionary<TKey, TValue>は、
     // IEnumerable<KeyValuePair<TKey, TValue>>を実装しているので
