b3a0ed8f69413c72278f0c0523787fb8be499203
伸悠 小林 <oibc37152@ojs.ac.jp>
2026-07-07 13:08:52 +0900

    経過日数完成

--- Chapter09/Section01/Form1.Designer.cs
+++ Chapter09/Section01/Form1.Designer.cs
@@ -26,6 +26,14 @@
             dtpDate = new DateTimePicker();
             btGet = new Button();
             tbOut = new TextBox();
+            nudDay = new NumericUpDown();
+            label1 = new Label();
+            dtpBirth = new DateTimePicker();
+            btBirthCalc = new Button();
+            tbOut1 = new TextBox();
+            label2 = new Label();
+            tbOut2 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)nudDay).BeginInit();
             SuspendLayout();
             // 
             // dtpDate
@@ -42,27 +50,97 @@
             btGet.Name = "btGet";
             btGet.Size = new Size(98, 39);
             btGet.TabIndex = 1;
-            btGet.Text = "取得";
+            btGet.Text = "計算";
             btGet.UseVisualStyleBackColor = true;
             btGet.Click += btGet_Click;
             // 
             // tbOut
             // 
-            tbOut.Location = new Point(12, 110);
+            tbOut.Font = new Font("Yu Gothic UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            tbOut.Location = new Point(12, 191);
             tbOut.Name = "tbOut";
-            tbOut.Size = new Size(164, 23);
+            tbOut.Size = new Size(464, 39);
             tbOut.TabIndex = 2;
+            // 
+            // nudDay
+            // 
+            nudDay.Font = new Font("Yu Gothic UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            nudDay.Location = new Point(101, 91);
+            nudDay.Name = "nudDay";
+            nudDay.Size = new Size(82, 39);
+            nudDay.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            label1.Location = new Point(189, 98);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 32);
+            label1.TabIndex = 4;
+            label1.Text = "日後";
+            // 
+            // dtpBirth
+            // 
+            dtpBirth.Font = new Font("Yu Gothic UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            dtpBirth.Location = new Point(12, 321);
+            dtpBirth.Name = "dtpBirth";
+            dtpBirth.Size = new Size(200, 39);
+            dtpBirth.TabIndex = 0;
+            // 
+            // btBirthCalc
+            // 
+            btBirthCalc.BackColor = SystemColors.ButtonHighlight;
+            btBirthCalc.Font = new Font("Yu Gothic UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            btBirthCalc.Location = new Point(233, 321);
+            btBirthCalc.Name = "btBirthCalc";
+            btBirthCalc.Size = new Size(75, 39);
+            btBirthCalc.TabIndex = 5;
+            btBirthCalc.Text = "計算";
+            btBirthCalc.UseVisualStyleBackColor = false;
+            btBirthCalc.Click += btBirthCalc_Click;
+            // 
+            // tbOut1
+            // 
+            tbOut1.Location = new Point(28, 412);
+            tbOut1.Name = "tbOut1";
+            tbOut1.Size = new Size(448, 23);
+            tbOut1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(28, 305);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 15);
+            label2.TabIndex = 7;
+            label2.Text = "生年月日";
+            // 
+            // tbOut2
+            // 
+            tbOut2.Location = new Point(16, 453);
+            tbOut2.Name = "tbOut2";
+            tbOut2.Size = new Size(467, 23);
+            tbOut2.TabIndex = 8;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1121, 631);
+            ClientSize = new Size(514, 631);
+            Controls.Add(tbOut2);
+            Controls.Add(label2);
+            Controls.Add(tbOut1);
+            Controls.Add(btBirthCalc);
+            Controls.Add(label1);
+            Controls.Add(nudDay);
             Controls.Add(tbOut);
             Controls.Add(btGet);
+            Controls.Add(dtpBirth);
             Controls.Add(dtpDate);
             Name = "Form1";
             Text = "開く";
+            ((System.ComponentModel.ISupportInitialize)nudDay).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -72,5 +150,12 @@
         private DateTimePicker dtpDate;
         private Button btGet;
         private TextBox tbOut;
+        private NumericUpDown nudDay;
+        private Label label1;
+        private DateTimePicker dtpBirth;
+        private Button btBirthCalc;
+        private TextBox tbOut1;
+        private Label label2;
+        private TextBox tbOut2;
     }
 }
--- Chapter09/Section01/Form1.cs
+++ Chapter09/Section01/Form1.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace Section01 {
     public partial class Form1 : Form {
         public Form1() {
@@ -5,40 +7,16 @@ namespace Section01 {
         }
 
         private void btGet_Click(object sender, EventArgs e) {
-            DateTime dt1 = dtpDate.Value;
+            DateTime date = dtpDate.Value;
+            tbOut.Text = date.AddDays((double)nudDay.Value).ToString();
+        }
+
+        private void btBirthCalc_Click(object sender, EventArgs e) {
+            DateTime birth = dtpBirth.Value;  //¶‚Ü‚ê‚½“ú•t
+            DateTime today = DateTime.Today; //¡“ú‚Ì“ú•t
+            tbOut1.Text = $"{(today.Year - birth.Year)}Î‚Å‚·";
             
-
-            if (DateTime.IsLeapYear(dt1.Year)) {
-                tbOut.Text = "‚¤‚é‚¤”N‚Å‚·";
-            } else {
-                tbOut.Text = "‚¤‚é‚¤”N‚Å‚Í‚ ‚è‚Ü‚¹‚ñ";
-            }
-
-
-            switch (dt1.DayOfWeek) {
-                case DayOfWeek.Saturday:
-                    tbOut.Text = "¡“ú‚Í“y—j“ú‚Å‚·";
-                    break;
-
-                case DayOfWeek.Sunday:
-                    tbOut.Text = "¡“ú‚Í“ú—j“ú‚Å‚·";
-                    break;
-                case DayOfWeek.Monday:
-                    tbOut.Text = "¡“ú‚ÍŒ—j“ú‚Å‚·";
-                    break;
-                case DayOfWeek.Tuesday:
-                    tbOut.Text = "¡“ú‚Í‰Î—j“ú‚Å‚·";
-                    break;
-                case DayOfWeek.Wednesday:
-                    tbOut.Text = "¡“ú‚Í…—j“ú‚Å‚·";
-                    break;
-                case DayOfWeek.Thursday:
-                    tbOut.Text = "¡“ú‚Í–Ø—j“ú‚Å‚·";
-                    break;
-                case DayOfWeek.Friday:
-                    tbOut.Text = "¡“ú‚Í‹à—j“ú‚Å‚·";
-                    break;
-            }
+            tbOut2.Text = $"{today.Date - birth.Date}“úŒo‰ß";
         }
     }
 }
